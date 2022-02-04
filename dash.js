dd83d1587ad86598970ecad08a1981e946aeebfb
arthurdavi10 <67353507+arthurdavi10@users.noreply.github.com>
2022-02-03 23:28:55 -0300

    Update dash.js

--- dash.js
+++ dash.js
@@ -5,7 +5,7 @@ function sendToServer(state) {
     //criar um objeto capaz de enviar dados via requisição HTTP GET
     const http = new XMLHttpRequest();
     //prepara um GET passando a váriavel lux como ultimo paramentro do link
-    http.open("GET","https://api.thingspeak.com/update?api_key="+KEY+"&field1=0"+state);
+    http.open("GET", "https://api.thingspeak.com/update?api_key="+ KEY +"&field1=0"+ state);
     //envia um GET
     http.send();
     //quando a requisição retornar ele chama o console e imprime o valor gerado
